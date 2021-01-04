eeb491337ac4dc5f489cdee1c0ca3dc9be14d504
Yvonne Chen <yvonnechen1991@gmail.com>
2021-01-04 00:23:15 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -7,7 +7,7 @@ def game_hash
     home: {
         team_name: "Brooklyn Nets",
         colors: ["Black", "White"],
-        players:  
+        players: [ 
           "Alan Anderson" => {
             number: 0,
             shoe: 16,
@@ -58,12 +58,12 @@ def game_hash
             blocks: 11,
             slam_dunks: 1
         }
-      }
+      ]
     },
       away: {
         team_name: "Charlotte Hornets",
         colors: ["Turquoise", "Purple"],
-        players: {
+        players: [
           "Jeff Adrien" => {
             number: 4,
             shoe: 18,
@@ -114,7 +114,7 @@ def game_hash
             blocks: 5,
             slam_dunks: 12
         }
-      }
+      ]
     }
   }
 end  
